83c1289668aa8a95bcebf73472a6ca035e3d332a
Samuel Wehrli <samuel.wehrli@gmail.com>
2025-02-26 06:39:57 +0100

    Added author

--- Start.py
+++ Start.py
@@ -10,4 +10,8 @@ st.title("Meine erste Streamlit App")
 Diese App wurde von folgenden Personen entwickelt:
 - Anna Müller (anmu@zhaw.ch)
 - Peter Meier (peme@zhaw.ch)
+
+Diese App ist das leere Gerüst für die App-Entwicklung im Modul Informatik 2 (BMLD/ZHAW)
+
+Autor: Samuel Wehrli (wehs@zhaw.ch)
 """
