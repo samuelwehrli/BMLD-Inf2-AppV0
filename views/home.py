9dcd47f77dbd4d3537c53ef283793d6e3214e162
Samuel Wehrli <samuel.wehrli@gmail.com>
2026-02-19 15:28:12 +0100

    mit refactored methoden

--- views/home.py
+++ views/home.py
@@ -1,11 +1,9 @@
 import streamlit as st
-import pandas as pd
 
 st.title("Meine erste Streamlit App")
 
 # !! WICHTIG: Eure Emails müssen in der App erscheinen!!
 
-# Streamlit über den Text unten direkt in die App - cool!
 """
 Diese App wurde von folgenden Personen entwickelt:
 - Anna Müller (anmu@zhaw.ch)
--- views/unterseite_a.py
+++ views/unterseite_a.py
@@ -3,4 +3,3 @@ import streamlit as st
 st.title("Unterseite A")
 
 st.write("Diese Seite ist eine Unterseite der Startseite.")
-
